210303791ee17a55de2261cda6187734dfb696ec
Lukas Ziefle <lziefle@ioet.com>
2018-09-26 10:41:42 -0500

    added missing crud methods

--- src/main/java/com/ioet/bpm/people/boundaries/PersonController.java
+++ src/main/java/com/ioet/bpm/people/boundaries/PersonController.java
@@ -6,6 +6,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+import java.util.Optional;
+
 @RestController
 @RequestMapping("/people")
 public class PersonController {
@@ -17,14 +20,49 @@ public class PersonController {
     }
 
     @GetMapping
-    public ResponseEntity<Iterable> listAll() {
-        Iterable<Person> skills = this.personRepository.findAll();
-        return new ResponseEntity<>(skills, HttpStatus.OK);
+    public ResponseEntity<Iterable> getAllPersons() {
+        Iterable<Person> persons = this.personRepository.findAll();
+        return new ResponseEntity<>(persons, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Person> getPerson(@PathVariable(value = "id") String personId) {
+        Optional<Person> personOptional = personRepository.findById(personId);
+        return personOptional.map(
+                person -> new ResponseEntity<>(person, HttpStatus.OK))
+                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
     @PostMapping
-    public ResponseEntity<Person> create(@RequestBody Person person) {
-        Person skillCreated = personRepository.save(person);
-        return new ResponseEntity<>(skillCreated, HttpStatus.CREATED);
+    public ResponseEntity<Person> createPerson(@RequestBody Person person) {
+        Person personCreated = personRepository.save(person);
+        return new ResponseEntity<>(personCreated, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deletePerson(@PathVariable(value = "id") String personId) {
+        Optional<Person> person = personRepository.findById(personId);
+        if (!person.isPresent()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        personRepository.delete(person.get());
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Person> updatePerson(@PathVariable(value = "id") String personId,
+                                             @Valid @RequestBody Person personDetails) {
+
+        Optional<Person> personOptional = personRepository.findById(personId);
+        if (!personOptional.isPresent()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        Person person = personOptional.get();
+        person.setName(personDetails.getName());
+        person.setAuthenticationIdentity(personDetails.getAuthenticationIdentity());
+        personRepository.save(person);
+        return new ResponseEntity<>(person, HttpStatus.OK);
     }
 }
