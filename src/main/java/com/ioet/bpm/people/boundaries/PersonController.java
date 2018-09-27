9d9813a3d160870957021ad5c2c2da13b61839f7
Jorge Luis Malla Sanchez <jmalla@ioet.com>
2018-09-27 12:00:04 -0500

    fix-password-people: Implements encrypt password

--- src/main/java/com/ioet/bpm/people/boundaries/PersonController.java
+++ src/main/java/com/ioet/bpm/people/boundaries/PersonController.java
@@ -1,17 +1,33 @@
 package com.ioet.bpm.people.boundaries;
 
+import com.google.common.hash.Hashing;
 import com.ioet.bpm.people.domain.Person;
 import com.ioet.bpm.people.repositories.PersonRepository;
+import org.bouncycastle.util.encoders.Base64;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
 import javax.validation.Valid;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.spec.InvalidKeySpecException;
 import java.util.Optional;
 
 @RestController
 @RequestMapping("/people")
 public class PersonController {
+
+    public static final String PBKDF2_ALGORITHM = "PBKDF2WithHmacSHA1";
+
+    // These constants may be changed without breaking existing hashes.
+    public static final int SALT_BYTE_SIZE = 24;
+    public static final int HASH_BYTE_SIZE = 18;
+    public static final int PBKDF2_ITERATIONS = 64000;
 
     private final PersonRepository personRepository;
 
@@ -35,6 +51,7 @@ public class PersonController {
 
     @PostMapping
     public ResponseEntity<Person> createPerson(@RequestBody Person person) {
+        person.setPassword(encryptPassword(person.getPassword()));
         Person personCreated = personRepository.save(person);
         return new ResponseEntity<>(personCreated, HttpStatus.CREATED);
     }
@@ -51,17 +68,53 @@ public class PersonController {
 
     @PutMapping("/{id}")
     public ResponseEntity<Person> updatePerson(@PathVariable(value = "id") String personId,
-                                             @Valid @RequestBody Person personToUpdate) {
+                                               @Valid @RequestBody Person personToUpdate) {
 
         Optional<Person> personFound = personRepository.findById(personId);
 
         if (personFound.isPresent()) {
             personToUpdate.setId(personId);
+            personToUpdate.setPassword(encryptPassword(personToUpdate.getPassword()));
             Person updatedPerson = personRepository.save(personToUpdate);
             return new ResponseEntity<>(updatedPerson, HttpStatus.OK);
         }
         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
 
+    }
+
+    private String encryptPassword(String password) {
+        SecureRandom random = new SecureRandom();
+        byte[] salt = new byte[SALT_BYTE_SIZE];
+        random.nextBytes(salt);
+
+        byte[] hash = pbkdf2(password.toCharArray(), salt, PBKDF2_ITERATIONS, HASH_BYTE_SIZE);
+        int hashSize = hash.length;
+
+        String parts = "sha1:" +
+                PBKDF2_ITERATIONS +
+                ":" + hashSize +
+                ":" +
+                Base64.toBase64String(salt) +
+                ":" +
+                Base64.toBase64String(hash);
+        return parts;
 
     }
+
+    private static byte[] pbkdf2(char[] password, byte[] salt, int iterations, int bytes)
+
+    {
+        try {
+            PBEKeySpec spec = new PBEKeySpec(password, salt, iterations, bytes * 8);
+            SecretKeyFactory skf = SecretKeyFactory.getInstance(PBKDF2_ALGORITHM);
+            return skf.generateSecret(spec).getEncoded();
+        } catch (NoSuchAlgorithmException ex) {
+            ex.printStackTrace();
+        } catch (InvalidKeySpecException ex) {
+            ex.printStackTrace();
+        }
+        return null;
+    }
+
+
 }
