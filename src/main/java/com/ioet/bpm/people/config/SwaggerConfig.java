96845347b31105717def136dacd687ff4bd63dc9
oemel09 <github@oemel09.de>
2018-10-24 16:26:22 -0500

    extended swagger config so that client code can be autogenerated

--- src/main/java/com/ioet/bpm/people/config/SwaggerConfig.java
+++ src/main/java/com/ioet/bpm/people/config/SwaggerConfig.java
@@ -2,24 +2,51 @@ package com.ioet.bpm.people.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.PathSelectors;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.util.HashSet;
+
+import static springfox.documentation.builders.PathSelectors.regex;
+
 @Configuration
 @EnableSwagger2
-public class SwaggerConfig {
+public class SwaggerConfig extends WebMvcConfigurationSupport {
 
     @Bean
     public Docket api() {
+        HashSet<String> protocols = new HashSet<>();
+        protocols.add("http");
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any())
+                .paths(regex("/people.*"))
+                .build()
+                .apiInfo(metaData())
+                .protocols(protocols);
+    }
+
+    private ApiInfo metaData() {
+        return new ApiInfoBuilder()
+                .title("BPM People API")
+                .description("API with CRUD methods for the bpm-people-project")
+                .version("0.0.1")
                 .build();
     }
 
+    @Override
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
 
 }
