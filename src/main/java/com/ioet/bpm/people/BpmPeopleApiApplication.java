58c2f0e7c08bc1be0c56790ce8322afd94569dbe
datmau <alex.cam.mol@gmail.com>
2019-01-11 11:28:45 -0500

    BPM-88 Add gradle Lambda deploy task, create lambda handler.

--- src/main/java/com/ioet/bpm/people/BpmPeopleApiApplication.java
+++ src/main/java/com/ioet/bpm/people/BpmPeopleApiApplication.java
@@ -2,13 +2,44 @@ package com.ioet.bpm.people;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.HandlerAdapter;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+import org.springframework.web.servlet.HandlerMapping;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 
-@EnableDiscoveryClient
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 @SpringBootApplication
-public class BpmPeopleApiApplication {
+public class BpmPeopleApiApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BpmPeopleApiApplication.class, args);
-	}
+
+    @Bean
+    public HandlerMapping handlerMapping() {
+        return new RequestMappingHandlerMapping();
+    }
+
+    @Bean
+    public HandlerAdapter handlerAdapter() {
+        return new RequestMappingHandlerAdapter();
+    }
+
+    @Bean
+    public HandlerExceptionResolver handlerExceptionResolver() {
+        return new HandlerExceptionResolver() {
+            @Override
+            public ModelAndView resolveException(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) {
+                return null;
+            }
+        };
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(BpmPeopleApiApplication.class, args);
+    }
 }
