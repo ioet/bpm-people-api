99e0dcaa42a8035cac054f194a9abe54f3dedf7c
datmau <alex.cam.mol@gmail.com>
2019-01-17 11:40:02 -0500

    handler class unittest

--- src/test/java/com/ioet/bpm/people/handlers/PeopleApiLambdaHandlerTest.java
+++ src/test/java/com/ioet/bpm/people/handlers/PeopleApiLambdaHandlerTest.java
@@ -1,28 +1,53 @@
 package com.ioet.bpm.people.handlers;
 
 
-import com.amazonaws.serverless.proxy.spring.SpringBootLambdaContainerHandler;
 import com.amazonaws.services.lambda.runtime.Context;
-import com.ioet.bpm.people.BpmPeopleApiApplication;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.junit.MockitoJUnitRunner;
-import org.springframework.mock.env.MockEnvironment;
-import software.amazon.ion.IonException;
+import org.junit.contrib.java.lang.system.EnvironmentVariables;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
-
+import java.io.OutputStream;;
 import static org.mockito.Mockito.*;
+import static org.powermock.api.mockito.PowerMockito.verifyNew;
+import static org.powermock.api.mockito.PowerMockito.whenNew;
 
-@RunWith(MockitoJUnitRunner.class)
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(PeopleApiLambdaHandler.class)
 public class PeopleApiLambdaHandlerTest {
 
     @Mock
     PeopleApiLambdaHandler peopleApiLambdaHandler;
+
+    @Rule
+    public final EnvironmentVariables environmentVariables
+            = new EnvironmentVariables();
+
+    @Test
+    public void setEnvironmentVariable() throws Exception {
+        environmentVariables.set("AWS_ACCESS_KEY_ID", "AKIAJV3HQDU44HTJTUYA");
+        environmentVariables.set("AWS_SECRET_ACCESS_KEY", "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");
+        environmentVariables.set("AWS_REGION", "us-east-1");
+        InputStream inputStream = mock(InputStream.class);
+        OutputStream outputStream = mock(OutputStream.class);
+        Context context = mock(Context.class);
+        PeopleApiLambdaHandler expectedHandler = mock(PeopleApiLambdaHandler.class);
+
+        whenNew(PeopleApiLambdaHandler.class).withNoArguments().thenReturn(expectedHandler);
+
+        PeopleApiLambdaHandler actualhandler = new PeopleApiLambdaHandler();
+
+        PowerMockito.verifyNew(PeopleApiLambdaHandler.class).withNoArguments();
+    }
+
+
 
     @Test
     public void lambdaHandlerClassIsUsed() throws IOException {
