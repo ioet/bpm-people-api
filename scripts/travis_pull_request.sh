a15318e6302e3e4eedcf390af2c75d93658428f0
Rene Enriquez <renriquez@grubhub.com>
2019-03-12 15:40:51 -0500

    Removing other stacks

--- scripts/travis_pull_request.sh
+++ scripts/travis_pull_request.sh
@@ -4,17 +4,19 @@ echo "Travis pull_request job"
 npm install
 npm run build
 pulumi stack select staging
+pulumi update --yes
 
 # Preview changes that would be made if the PR were merged.
-case ${TRAVIS_BRANCH} in
-    master)
-        pulumi update --yes
-        ;;
+# case ${TRAVIS_BRANCH} in
+#    master)
+#        pulumi stack select staging
+#        pulumi update --yes
+#        ;;
     #production)
     #    pulumi stack select acme/website-production
     #    pulumi preview
     #    ;;
     *)
-        echo "No Pulumi stack targeted by pull request branch ${TRAVIS_BRANCH}."
-        ;;
-esac
+#        echo "No Pulumi stack targeted by pull request branch ${TRAVIS_BRANCH}."
+#        ;;
+# esac
--- scripts/travis_push.sh
+++ scripts/travis_push.sh
@@ -3,18 +3,18 @@ echo "Travis push job"
 # Download dependencies and build
 npm install
 npm run build
-pulumi stack select staging
 
 # Update the stack
-case ${TRAVIS_BRANCH} in
-    master)
-        pulumi update --yes
-        ;;
+# case ${TRAVIS_BRANCH} in
+#    master)
+#        pulumi stack select staging
+#        pulumi update --yes
+#        ;;
     #production)
     #    pulumi stack select bpm-people-api-ops/website-production
     #    pulumi update --yes
     #    ;;
-    *)
-        echo "No Pulumi stack associated with branch ${TRAVIS_BRANCH}."
-        ;;
-esac
+#    *)
+#        echo "No Pulumi stack associated with branch ${TRAVIS_BRANCH}."
+#        ;;
+# esac
